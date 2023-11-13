1174a088fbd47828a6d68a536c715a6a6df25e61
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-11-13 16:52:55 +0900

    Create try.py

---
+++ autd3/try.py
@@ -0,0 +1,56 @@
+from pyautd3 import AUTD3, Controller, Silencer
+from pyautd3.link.soem import SOEM, OnErrFunc
+from pyautd3.gain import Focus
+from pyautd3.modulation import Sine
+
+import numpy as np
+
+import os
+import ctypes
+
+import time
+import math
+
+def on_lost(msg: ctypes.c_char_p):
+    print(msg.decode("utf-8"), end="")
+    os._exit(-1)
+
+
+if __name__ == "__main__":
+    on_lost_func = OnErrFunc(on_lost)
+
+    autd = (
+        Controller.builder()
+        .add_device(AUTD3.from_euler_zyz([0.0, 0.0, 0.0], [0.0, 0.0, 0.0]))
+        .open_with(SOEM.builder().with_on_lost(on_lost_func))
+    )
+
+    firm_info_list = autd.firmware_info_list()
+    print("\n".join([f"[{i}]: {firm}" for i, firm in enumerate(firm_info_list)]))
+
+    autd.send(Silencer())
+
+    t = 0
+    omega = 0.01
+    r = 50
+    x = 0.0
+    y = 0.0
+    z = 150.0
+    z_0 = 150.0
+    while True:
+        t += omega
+        """
+        z = 100*(1+math.sin(t))
+        x = r*math.cos(t)
+        y = r*math.sin(t)
+        """
+        time.sleep(omega)
+        print(f"t={t},x={x},y={y},z={z}")
+
+        g = Focus(autd.geometry.center + np.array([x, y, z]))
+        m = Sine(150)
+        autd.send((m, g))
+
+    _ = input()
+
+    autd.close()
