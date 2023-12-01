7096c30642ca842dc8691718b0fbb041553d4082
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-12-01 23:40:51 +0900

    Update func.py

--- matplot/func.py
+++ matplot/func.py
@@ -1,14 +1,39 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-import math
 
 
 def func(x):#両対数軸に描画する関数
-    return np.sqrt(x)
+    #return 80*(x**(-0.8)) + 0.08
+    return 40*(x**(-0.65))#全体的に近い直線
+    #return 100*(x**(-0.85))#先頭が正確
+    #return 30*(x**(-0.6))#後ろが正確
+    #return 3*(10*(x**(-0.85)) + 8*(x**(-0.6)))
+    
+"""def func_2(x):#両対数における折れ線近似
+    if x < 100:
+        return 100*(x**(-0.85))
+    elif 100 <= x < 200:
+        return 40*(x**(-0.65))
+        #return 39*(x**(-0.65))
+    elif 200 <= x < 1000:
+        return 35*(x**(-0.62))
+        #return 32*(x**(-0.61))
+        #return 35*(x**(-0.63))
+        #return 37*(x**(-0.63))
+    elif 1000 <= x:
+        #return 30*(x**(-0.6))
+        #return 32*(x**(-0.61))
+        return 35*(x**(-0.62))"""
+
+def func_2(x):#両対数における折れ線近似
+    if x < 100:
+        return 101*(x**(-0.85))
+    else:
+        return 35*(x**(-0.62))
 
 
-background_image = mpimg.imread('a.png')#画像の読み込み
+background_image = mpimg.imread('b.png')#画像の読み込み
 
 fig, ax = plt.subplots()
 
@@ -32,6 +57,11 @@ ax_log.set_ylim(0.1, 100)
 #関数を描写
 x_values = np.logspace(np.log10(10), np.log10(10000), 1000)
 y_values = func(x_values)
+#####
+y_values = []
+for i in x_values:
+    y_values.append(func_2(i))
+#####
 ax_log.plot(x_values, y_values, label='func(x) = sqrt(x)', color='blue')
 
 ax_log.grid(True)
