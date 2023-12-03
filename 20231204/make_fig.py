dbf1a2807fb084169175bd1e19783cde9b0232c0
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-12-04 00:28:24 +0900

    Update make_fig.py

--- 20231204/make_fig.py
+++ 20231204/make_fig.py
@@ -13,24 +13,35 @@ def from_N_make_ppm(N_air, N_alc):
             return (35/R)**(1/0.62)
 
 
-N_air = 252
+#N_air = 252
 y_lim = 5000
 y_valid_min = 50
-csv_file = "data.csv"
+#csv_file = "data.csv"
 if __name__ == "__main__":
+    N_air = float(input("空気中の10bitの数値を入力："))
+    csv_file_number = int(input("読み込みたいcsvファイルの数を入力："))
+    csv_file_name = []
+    date_name = []
+    for i in range(csv_file_number):
+        temp = input(f"{i+1}個目のcsvファイルの名前を入力：")
+        csv_file_name.append(temp)
+        temp = input(f"{i+1}個目のcsvファイルの凡例を入力：")
+        date_name.append(temp)
     get_time = float(input("サンプルに要した時間[s]を入力："))
     y_scale = int(input("線形軸→0，対数軸→1："))
-    df = pd.read_csv(csv_file, header=None)
-    df = df.iloc[:, 0].values
-    data_number = len(df)
-    f = get_time / data_number#サンプリング周期[s]
-    t = []
-    value = []
-    for i in range(data_number):
-        t.append(i*f)
-        value.append(from_N_make_ppm(N_air, df[i]))
     
-    max_value = max(value)
+    for i in range(csv_file_number):
+        df = pd.read_csv(csv_file_name[i], header=None)
+        df = df.iloc[:, 0].values
+        data_number = len(df)
+        f = get_time / data_number#サンプリング周期[s]
+        t = []
+        value = []
+        for j in range(data_number):
+            t.append(j*f)
+            value.append(from_N_make_ppm(N_air, df[j]))
+            plt.plot(t, value, marker="o", linestyle="", label = date_name[i], color ="blue")
+    
 
     fig, ax = plt.subplots()
     ax.set_xlim(0, get_time)
@@ -42,21 +53,13 @@ if __name__ == "__main__":
         y_flag = int(input("y軸の上限を固定する→0，しない→1："))
         if y_flag == 0:
             ax.set_ylim(0, y_lim)
-        elif y_flag == 1:
-            ax.set_ylim(0, max_value + 1000)
-            if max_value > y_lim:
-                plt.plot([0, get_time], [y_lim, y_lim], linestyle="--", label="max valid value", color="red")
 
     plt.xlabel("time [s]")
     plt.ylabel("concentration [ppm]")
     plt.title("ppm-t")
-
-    plt.plot(t, value, marker="o", linestyle="", label="concentration", color ="blue")
+    
     plt.plot([0, get_time], [y_valid_min, y_valid_min], linestyle="--", label="lowest valid value", color="red")
     
-    title_position = plt.gca().title.get_position()
-    plt.text(title_position[0] + 0.2, title_position[1] + 0.05, f"max value = {max_value:.1f} [ppm]\nminimum value = {min(value):.1f} [ppm]", color="black", transform=plt.gca().transAxes)
-
     plt.legend(loc="upper left")
 
     ax.grid(True)
