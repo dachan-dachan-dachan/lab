984a686ba2b76dd98e6a50befbacdda7c2e0de76
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-12-03 23:39:54 +0900

    Update ppm-time_fig.py

--- matplot/ppm-time_fig.py
+++ matplot/ppm-time_fig.py
@@ -29,25 +29,35 @@ if __name__ == "__main__":
     for i in range(data_number):
         t.append(i*f)
         value.append(from_N_make_ppm(N_air, df[i]))
+    
+    max_value = max(value)
 
     fig, ax = plt.subplots()
     ax.set_xlim(0, get_time)
     if y_scale == 1:
         base = float(input("対数の底を入力："))
-        plt.yscale('log', base=base)
+        plt.yscale("log", base=base)
+        ax.set_ylim(10, 10000)
     elif y_scale == 0:
         y_flag = int(input("y軸の上限を固定する→0，しない→1："))
         if y_flag == 0:
             ax.set_ylim(0, y_lim)
+        elif y_flag == 1:
+            ax.set_ylim(0, max_value + 1000)
+            if max_value > y_lim:
+                plt.plot([0, get_time], [y_lim, y_lim], linestyle="--", label="max valid value", color="red")
 
-    plt.xlabel('time [s]')
-    plt.ylabel('concentration [ppm]')
-    plt.title('ppm-t')
+    plt.xlabel("time [s]")
+    plt.ylabel("concentration [ppm]")
+    plt.title("ppm-t")
 
+    plt.plot(t, value, marker="o", linestyle="", label="concentration", color ="blue")
+    plt.plot([0, get_time], [y_valid_min, y_valid_min], linestyle="--", label="lowest valid value", color="red")
+    
+    title_position = plt.gca().title.get_position()
+    plt.text(title_position[0] + 0.2, title_position[1] + 0.05, f"max value = {max_value:.1f} [ppm]\nminimum value = {min(value):.1f} [ppm]", color="black", transform=plt.gca().transAxes)
 
-    plt.plot(t, value, marker="o", linestyle='', label="concentration", color ="blue")
-    plt.plot([0, get_time], [y_valid_min, y_valid_min], linestyle='--', label="lowest valid value", color="red")
-    plt.legend(loc='upper left')
+    plt.legend(loc="upper left")
 
     ax.grid(True)
 
