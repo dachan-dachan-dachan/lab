2f8a26347a28a7075b58d6265eb5262970f060dc
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-12-08 21:56:10 +0900

    Update make_fig.py

--- 20231208-11/sensor_time_react/make_fig.py
+++ 20231208-11/sensor_time_react/make_fig.py
@@ -2,22 +2,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot(csv_file_name, color, N_air, label):
-    df = pd.read_csv(csv_file_name, header=None)
-    df = df.iloc[:, 0].values
-    data_number = len(df)
-    f = get_time / data_number#サンプリング周期[s]
-    t = []
-    value = []
-    for i in range(data_number):
-        t.append(i*f)
-        value.append(from_N_make_ppm(N_air, df[i]))
-    plt.plot(t, value, marker="o", linestyle="", label = label, color=color)
-
-#N_air = 252
 y_lim = 1023
 y_valid_min = 0
-csv_file_name = "l.csv"
+first_csv_file_name = "l.csv"
+second_csv_file_name = "l.csv"
+third_csv_file_name = "l.csv"
 get_time = 180
 label = "left"
 color = "blue"
