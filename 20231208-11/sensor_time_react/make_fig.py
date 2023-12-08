2206407ee7e98e1a4f9bd671875ee67d21733a87
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-12-08 22:16:49 +0900

    Update make_fig.py

--- 20231208-11/sensor_time_react/make_fig.py
+++ 20231208-11/sensor_time_react/make_fig.py
@@ -4,12 +4,9 @@ import pandas as pd
 
 y_lim = 1023
 y_valid_min = 0
-first_csv_file_name = "l.csv"
-second_csv_file_name = "l.csv"
-third_csv_file_name = "l.csv"
-get_time = 180
-label = "left"
-color = "blue"
+first_csv_file_name = "20231208_react_time_1.csv"
+second_csv_file_name = "20231208_react_time_2.csv"
+third_csv_file_name = "20231208_react_time_3.csv"
 if __name__ == "__main__":
     fig, ax = plt.subplots()
     ax.set_xlim(0, get_time)
@@ -19,15 +16,51 @@ if __name__ == "__main__":
     plt.ylabel("concentration level [-]")
     plt.title("Time Diagram")
     
-    #df = pd.read_csv(csv_file_name, header=None)
-    df = pd.read_csv(csv_file_name)
+    y_value = []
+    get_time = []
+
+    df = pd.read_csv(first_csv_file_name, header=None)
     df = df.iloc[1:, 0].values
-    data_number = len(df)
-    f = get_time / data_number#サンプリング周期[s]
+    y_value.append(df)
+    get_time.append(df[-1])
+    del y_value[-1]
+
+    df = pd.read_csv(second_csv_file_name, header=None)
+    df = df.iloc[1:, 0].values
+    y_value.append(df)
+    get_time.append(df[-1])
+    del y_value[-1]
+
+    df = pd.read_csv(third_csv_file_name, header=None)
+    df = df.iloc[1:, 0].values
+    y_value.append(df)
+    get_time.append(df[-1])
+    del y_value[-1]
+
+    
+    data_number = len(y_value[0]) + len(y_value[1]) + len(y_value[2])
+    f = (get_time[0] + get_time[1] + get_time[2]) / data_number#サンプリング周期[s]
     t = []
+    t.append([])
+    t.append([])
+    t.append([])
     for i in range(data_number):
-        t.append(i*f)
-    plt.plot(t, df, marker="o", linestyle="", label = label, color = color)
+        if i*f <= get_time[0]:
+            t[0].append(i*f)
+        elif i*f <= get_time[1]:
+            t[1].append(i*f)
+        elif i*f <= get_time[2]:
+            t[2].append(i*f)
+
+    """
+    plt.scatter(t[0:len(y_value[0])], y_value[0], marker="o", linestyle="", label = "OFF", color = "red", s = 2)
+    plt.scatter(t[len(y_value[0]):len(y_value[1])], y_value[1], marker="o", linestyle="", label = "ON", color = "blue", s = 2)
+    plt.scatter(t[len(y_value[1]):len(y_value[2])], y_value[2], marker="o", linestyle="", color = "red", s = 2)
+    """
+    plt.scatter(t[0], y_value[0], marker="o", linestyle="", label = "OFF", color = "red", s = 2)
+    plt.scatter(t[1], y_value[1], marker="o", linestyle="", label = "ON", color = "blue", s = 2)
+    plt.scatter(t[2], y_value[2], marker="o", linestyle="", color = "red", s = 2)
+    
     plt.legend(loc="upper left")
 
     ax.grid(True)
