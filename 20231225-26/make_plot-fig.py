a40a4e6bd7c67d5436129e304064f3eb58aa448f
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-12-24 01:58:19 +0900

    Update make_plot-fig.py

--- 20231225-26/make_plot-fig.py
+++ 20231225-26/make_plot-fig.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     scatter_size = 3
     
     for k in range(-40, 50, 10):
-        file_name = f"20231218-ex1-x_O=30-x_L={k}.csv"
+        file_name = f"20231224-phi={k}_0.csv"
         df = pd.read_csv(file_name, header=None)
         df = df.iloc[0:, 0].values
         data_number = len(df)
@@ -27,19 +27,18 @@ if __name__ == "__main__":
         t = []
         for i in range(data_number):
             t.append(i*f)
-        plt.scatter(t, df, marker="o", label = f"x_L={k}", s=scatter_size)
-    
+        plt.scatter(t, df, marker="o", label = f"phi={k} with", s=scatter_size)
 
-    
-    file_name = "aaa.csv"
-    df = pd.read_csv(file_name, header=None)
-    df = df.iloc[0:, 0].values
-    data_number = len(df)
-    f = get_time / data_number
-    t = []
-    for i in range(data_number):
-        t.append(i*f)
-    plt.scatter(t, df, marker="o", label = f"x_L=40_non-directivity", s=scatter_size)
+    for k in range(-40, 50, 10):
+        file_name = f"20231224-phi={k}_1.csv"
+        df = pd.read_csv(file_name, header=None)
+        df = df.iloc[0:, 0].values
+        data_number = len(df)
+        f = get_time / data_number#サンプリング周期[s]
+        t = []
+        for i in range(data_number):
+            t.append(i*f)
+        plt.scatter(t, df, marker="o", label = f"phi={k} without", s=scatter_size)
     
 
     #plt.legend(loc="upper left", fontsize=17, ncol=2)
