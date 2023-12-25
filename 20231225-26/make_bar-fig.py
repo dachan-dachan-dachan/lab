30455200df9103313f22c102c580b9c12840f5a3
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-12-25 10:24:47 +0900

    Update make_bar-fig.py

--- 20231225-26/make_bar-fig.py
+++ 20231225-26/make_bar-fig.py
@@ -19,17 +19,10 @@ def upper(df):
 get_time = 100
 if __name__ == "__main__":
     fig, ax = plt.subplots()
-    #ax.set_xlim(0, get_time)
-    #ax.set_xlim(-50, 50)
-    #ax.set_xlim(-15, 15)
-    ax.set_ylim(y_valid_min, y_lim)
+    plt.ylim(y_valid_min, y_lim)
     #plt.xticks([10, 70, 100])
-
-    ax2 = ax.twinx()
-    ax2.set_ylim(y_valid_min, y_lim)
-
-    ax.set_xlabel("phi [degree]")
-    ax.set_ylabel("max concentration level [-]")
+    plt.xlabel("phi [degree]")
+    plt.ylabel("max concentration level [-]")
     plt.title("Odor Source phi=6")
 
     scatter_size = 200
@@ -39,45 +32,36 @@ if __name__ == "__main__":
     x_ticks = []
     for k in range(-12, 13, 6):
         x_ticks.append(k)
-        file_name = f"20231224-phi={k}_0.csv"
+        file_name = f"20231225_theat={theat_x_A}_with.csv"
         df = pd.read_csv(file_name, header=None)
         df = df.iloc[0:, 0].values
         data_number = len(df)
         x.append(k)
         y.append(max(df))
-    ax.bar(x, y, width = -2, align = "edge", label = "max value with [-]", color = "blue")
+    plt.bar(x, y, width = -2, align = "edge", label = "max value with [-]", color = "blue")
 
     for k in range(-12, 13, 6):
         #x_ticks.append(k)
-        file_name = f"20231224-phi={k}_1.csv"
+        file_name = f"20231225_theat={theat_x_A}_without.csv"
         df = pd.read_csv(file_name, header=None)
         df = df.iloc[0:, 0].values
         data_number = len(df)
         x.append(k)
         y.append(max(df))
-    ax2.bar(x, upper_t, width = 2, align = "edge", label = "max value without [-]", color = "red")
+    plt.bar(x, upper_t, width = 2, align = "edge", label = "max value without [-]", color = "red")
 
-    file_name = "20231224_1.csv"
+    file_name = "20231225_only.csv"
     df = pd.read_csv(file_name, header=None)
     df = df.iloc[0:, 0].values
     x = [-50, 50]
     y = [max(df), max(df)]
-    ax3 = ax.twinx()
-    ax3.set_ylim(y_valid_min, y_lim)
-    ax3.plot(x, y, label = "max value only [-]", marker = "", linestyle = "-", color = "green")
+    plt.plot(x, y, label = "max value only [-]", marker = "", linestyle = "-", color = "green")
     
     
     plt.xticks(x_ticks)
-    #plt.legend(loc="upper left", fontsize=17, ncol=5)
-    ax.legend(loc="upper left", fontsize=17, ncol=5)
-    ax2.legend(loc="upper right", fontsize=17, ncol=5)
+    plt.legend(loc="upper left", fontsize=17, ncol=5)
     #plt.legend(loc="upper left", fontsize=17)
 
-    ax.grid(True)
-    ax2.grid(True)
-
-    ax3.legend(loc="upper center", fontsize=17, ncol=5)
-    ax3.grid(True)
 
     # グラフの表示
     plt.show()
